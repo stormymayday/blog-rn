6abca10abf6609606d6720142c44fe2008c70bab
stormymayday <stormymayday7@gmail.com>
2024-05-29 20:25:58 -0700

    Adding Context

--- App.js
+++ App.js
@@ -2,6 +2,9 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+// Context
+import { BlogProvider } from "./src/context/BlogContext";
+
 // Screens
 import IndexScreen from "./src/screens/IndexScreen";
 
@@ -10,15 +13,16 @@ const Stack = createStackNavigator();
 
 export default function App() {
     return (
-        // Using the NavigationContainer to wrap all of our navigation
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Index">
-                <Stack.Screen
-                    name="Index"
-                    component={IndexScreen}
-                    options={{ title: "Index" }}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <BlogProvider>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Index">
+                    <Stack.Screen
+                        name="Index"
+                        component={IndexScreen}
+                        options={{ title: "Index" }}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </BlogProvider>
     );
 }
