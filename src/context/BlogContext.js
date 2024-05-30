7a4222202bea1d9fe04903c9b5425bdbc3c98bc3
stormymayday <stormymayday7@gmail.com>
2024-05-29 21:01:11 -0700

    Moving data with Context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,10 @@
 import React from "react";
 
+// Creating Context Object
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-    return <BlogContext.Provider>{children}</BlogContext.Provider>;
+    return <BlogContext.Provider value={5}>{children}</BlogContext.Provider>;
 };
+
+export default BlogContext;
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,9 +1,15 @@
 import { View, Text, StyleSheet } from "react-native";
 
+import BlogContext from "../context/BlogContext";
+import { useContext } from "react";
+
 const IndexScreen = () => {
+    const value = useContext(BlogContext);
+
     return (
         <View>
             <Text>Index Screen</Text>
+            <Text>{value}</Text>
         </View>
     );
 };
